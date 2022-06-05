e95ec3cdc9a8b12e4b936f847d62d64ec7da0547
jahidem <jahid.hossain.e99@gmail.com>
2022-06-05 12:13:32 +0000

    homebody don

--- src/components/Home/HomeBody.jsx
+++ src/components/Home/HomeBody.jsx
@@ -3,7 +3,7 @@ import ServiceContain from './ServiceContain'
 
 const HomeBody = () =>{
     return(<Flex flexDir="column">
-        <Flex alignSelf="center"  flexDir="row" bgColor="gray.100" p="1rem"
+        <Flex alignSelf="center"  flexDir="row" bgColor="gray.50" p="1rem"
             borderRadius="5px" marginTop="2rem"
             justifyContent="space-between" width="1000px">
         <Box margin="2rem" >
@@ -12,15 +12,32 @@ const HomeBody = () =>{
                 fontSize="48px"
                 p=" 0 1rem"
             >You are in <Text 
-            p="0 0 0 1rem"
+            p="0 0 0 0rem"
             color="#34ace0">safe hands</Text></Text>
-            <Text  fontSize="16px" p="1rem">A web application for making doctor appointments, patient scheduling and keeping track of medicine prescribed by the doctor and necessary diagnosis reports.
+            <Text  fontSize="18px"
+            color="gray.550"
+            p="1rem">A web application for making doctor appointments, patient scheduling and keeping track of medicine prescribed by the doctor and necessary diagnosis reports.
             </Text>
         </Box>
+        
+        <Box height="380px" width="70%">
         <Image src="images/doctor-14044.png" maxH="380px" 
             alt="doctor image"
-            
+            zIndex="5"
+            position="absolute"
             /> 
+        <Box 
+            bgColor="blue.700"
+            height="300px"
+            width="320px" 
+            borderRadius="10px"
+            zIndex="4"
+            rotate="45deg"
+            
+        >
+           </Box>
+            
+        </Box>
         </Flex>
         <Box>
             <Text>Our Services:</Text>
